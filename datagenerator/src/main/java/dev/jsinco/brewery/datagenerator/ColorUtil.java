71de9dbfad8f0c6cd5351256be59fbe6a747ebb0
Hjalmar Gunnarsson <officialhjalmar.gunnarsson@outlook.com>
2026-05-07 07:47:51 +0200

    fix(colors): Remove unused method

--- datagenerator/src/main/java/dev/jsinco/brewery/datagenerator/ColorUtil.java
+++ datagenerator/src/main/java/dev/jsinco/brewery/datagenerator/ColorUtil.java
@@ -7,22 +7,6 @@ import java.util.Comparator;
 
 public class ColorUtil {
 
-    public static Color getAverageColor(BufferedImage image) {
-        long sumr = 0, sumg = 0, sumb = 0;
-        double num = 0;
-        for (int x = 0; x < image.getWidth(); x++) {
-            for (int y = 0; y < image.getHeight(); y++) {
-                Color pixel = new Color(image.getRGB(x, y), true);
-                float alpha = (float) pixel.getAlpha() / 255;
-                sumr += (long) (pixel.getRed() * alpha);
-                sumg += (long) (pixel.getGreen() * alpha);
-                sumb += (long) (pixel.getBlue() * alpha);
-                num += alpha;
-            }
-        }
-        return new Color((int) (sumr / num), (int) (sumg / num), (int) (sumb / num));
-    }
-
     public static Color getDistinctColor(BufferedImage image) {
         int imageSize = 0;
         Bucket[] buckets = new Bucket[32];
